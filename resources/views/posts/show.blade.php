fbb7ea52e71376d9df32e9667309fa488842db15
nazimhira <nazimhossain89@student.sust.edu>
2020-07-02 22:01:22 +0600

    ''

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,20 +2,29 @@
 
 @section('content')
 
-<div class="container jumbotron mt-4">
-    <div class="row">
-        <div class="col-8">
-            <img src="/storage/{{ $post->image }}" class="w-100 pr-3">
-        </div>
-        <div class="col-4">
-
-            <div class="d-flex align-content-center">
-                <a href="/profile/{{ $post->user->id }}"><img src="/storage/{{ $post->user->profile->image }}" class="rounded-circle mr-1"
-                        style="max-width: 30px"></a>
-                <a href="/profile/{{ $post->user->id }}" class="text-dark" style="text-decoration: none"><h3>{{ $post->user->username }}</h3></a>
+    <div class="container-sm jumbotron">
+        <div class="row">
+            <div class="col-8">
+                <img src="/storage/{{ $post->image }}" class="w-100 pr-3">
             </div>
-            <p>{{ $post->caption }}</p>
+            <div class="col-4">
+
+                <div class="d-flex align-content-center">
+                    <a href="/profile/{{ $post->user->id }}"><img src="/storage/{{ $post->user->profile->image }}"
+                                                                  class="rounded-circle mr-1"
+                                                                  style="max-width: 30px"></a>
+                    <a href="/profile/{{ $post->user->id }}" class="text-dark"><h3>{{ $post->user->username }}</h3></a>
+                </div>
+                <hr>
+                <p>
+                <span class="font-weight-bold border-right mr-1 p-1">
+                    <a href="/profile/{{ $post->user->id }}" class="text-dark">
+                        {{ $post->user->username }}
+                    </a>
+                </span>
+                    {{ $post->caption }}
+                </p>
+            </div>
         </div>
     </div>
-</div>
 @endsection
